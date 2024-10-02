b4da6ee7f11c3853beab5963afa6f60388e82fc9
Vastvikta Dwaria <dvastvikta@gmail.com>
2024-10-02 16:10:02 +0530

    Add files via upload

---
+++ StrongNumber.java
@@ -0,0 +1,43 @@
+package ajp_java;
+
+import java.util.Scanner;
+
+public class StrongNumber {
+    // Method to calculate the factorial of a number
+    public static int factorial(int num) {
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    // Method to check if a number is a Strong number
+    public static boolean isStrongNumber(int number) {
+        int originalNumber = number;
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += factorial(digit);
+            number /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isStrongNumber(number)) {
+            System.out.println(number + " is a Strong number.");
+        } else {
+            System.out.println(number + " is not a Strong number.");
+        }
+
+        scanner.close();
+    }
+}
